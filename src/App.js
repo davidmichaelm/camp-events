3de2a6edf2cbdd45301bb4041e2a321d0da98a40
David Marquardt <davemarquardt@gmail.com>
2021-09-13 08:14:40 -0500

    Initial sanity integration and event card styles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,63 @@
+import {useEffect, useState} from "react";
+import BlockContent from "@sanity/block-content-to-react";
+import styles from "./eventCard.module.css";
+
 function App() {
-  return (
-    <div className="camp-events-app">
-        Testing
-    </div>
-  );
+    const [events, setEvents] = useState([]);
+
+    useEffect(() => {
+        const fetchEvents = async () => {
+            const groq = `*[_type == 'event' && active == true] {
+                active,
+                name,
+                "imageUrl": image.asset->url,
+                startDate,
+                endDate,
+                shortDescription
+            }`;
+            const query = `?query=${encodeURIComponent(groq)}`;
+            let response = await fetch(`https://m5ik5me8.api.sanity.io/v1/data/query/production${query}`);
+            response = await response.json();
+            console.log(response.result);
+            setEvents(response.result);
+        }
+
+        fetchEvents();
+    }, []);
+
+    console.log(styles);
+
+    return (
+        <div className={styles.campEventsApp}>
+            <h1 className={styles.eventsHeader}>What's going on at Camp?</h1>
+            {
+                events.map((e, index) => {
+                    return (
+                        <div key={index} className={styles.eventCard}>
+                            <img src={e.imageUrl} className={styles.image} />
+
+                            <div className={styles.eventCardHeader}>
+                                <h3 className={styles.eventCardTitle}>{e.name}</h3>
+                                <div className={styles.eventCardDate}>{new Date(e.startDate).toLocaleDateString("en", {
+                                    day: "numeric",
+                                    month: "long"
+                                })}</div>
+                            </div>
+
+                            <div className={styles.eventCardBody}>
+                                <BlockContent blocks={e.shortDescription} />
+                            </div>
+
+                            <div className={styles.eventButtons}>
+                                <div className={styles.eventButton}>Learn More</div>
+                                <div className={styles.eventButton}>Sign Up</div>
+                            </div>
+                        </div>
+                    );
+                })
+            }
+        </div>
+    );
 }
 
 export default App;
