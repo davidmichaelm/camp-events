ceafb5f5e4ed8441dcb869de658ed6fdc7699b89
David Marquardt <davemarquardt@gmail.com>
2021-09-18 19:53:32 -0500

    Don't show month twice if in the same month

--- src/components/EventCard.js
+++ src/components/EventCard.js
@@ -3,15 +3,7 @@ import BlockContent from "@sanity/block-content-to-react";
 import {DateTime, Interval} from "luxon";
 
 export const EventCard = ({name, imageUrl, startDate, endDate, shortDescription, buttons}) => {
-    startDate = DateTime.fromISO(startDate);
-    endDate = DateTime.fromISO(endDate);
-    const interval = Interval.fromDateTimes(startDate, endDate);
-
-    let dates;
-    if (interval.start.hasSame(interval.end, 'day'))
-        dates = interval.start.toFormat("MMMM d");
-    else
-        dates = interval.toFormat("MMMM d");
+    const dates = getDates(startDate, endDate)
 
     return (
         <div className={styles.eventCard}>
@@ -40,3 +32,20 @@ export const EventCard = ({name, imageUrl, startDate, endDate, shortDescription,
         </div>
     );
 };
+
+function getDates(startDate, endDate) {
+    startDate = DateTime.fromISO(startDate);
+    endDate = DateTime.fromISO(endDate);
+
+    const interval = Interval.fromDateTimes(startDate, endDate);
+
+    if (interval.start.hasSame(interval.end, 'day')) {
+        return interval.start.toFormat("MMMM d");
+    } else {
+        if (interval.start.hasSame(interval.end, 'month')) {
+            return interval.start.toFormat("MMMM d") + " – " + interval.end.toFormat("d");
+        } else {
+            return interval.toFormat("MMMM d");
+        }
+    }
+}
